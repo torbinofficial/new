e57c061ebead0d169d3cba11d7c3a7cea823daa1
Vyacheslav <45356117+torbinofficial@users.noreply.github.com>
2022-01-25 19:26:55 +0200

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -83,8 +83,8 @@ def list_channels(message):
         dropbox_download_file("/hitler-bot/Book.xlsx", "Book.xlsx")
         df = pd.read_excel('Book.xlsx', index_col=0)
         for item in range(0, df.shape[0]):
-            string = str(item) + ". \n" + df.iloc[item].to_string()
-            bot.send_message(message.chat.id, text = "Операция успешна")
+            stringg = str(item) + ". \n" + df.iloc[item].to_string()
+            bot.send_message(message.chat.id, text = stringg)
     except:
         bot.send_message(message.chat.id, text = "Произошла ошибка!")
  
