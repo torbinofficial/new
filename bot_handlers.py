1e686407c69b8aa62f8a4b9511287287998d6449
Vyacheslav <45356117+torbinofficial@users.noreply.github.com>
2022-01-31 15:56:13 +0200

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -265,7 +265,7 @@ def get_user_id(message):
 def get_channel_id(message):
     bot.send_message(message.chat.id, str(message.chat.id))
 
-@bot.channel_post_handler(content_types=types.ContentType.ANY)
+@bot.channel_post_handler(content_types=["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "location", "contact", "new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo", "group_chat_created", "supergroup_chat_created", "channel_chat_created", "migrate_to_chat_id", "migrate_from_chat_id" , "pinned_message"])
 def reposts(message):
     text = ""
 #     print(str(message.forward_from_chat.id))
