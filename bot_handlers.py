c6e0fcff053fd0f6b54a928e80bf6ba837b40d54
Vyacheslav <45356117+torbinofficial@users.noreply.github.com>
2022-01-31 15:50:14 +0200

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import telebot
 import dropbox
+from telebot import types 
+
 
 DROPBOX_TOKEN = "0c2FLOlpeLMAAAAAAAAAAb9obp9hAZUxInCh5qCRoUh2A094mQOTrCJLvvpNP4gx"
 TOKEN = "5298849535:AAEAcppc5zfA1D88YTOZGxPqaYLBUnOtNmE"
@@ -263,10 +265,7 @@ def get_user_id(message):
 def get_channel_id(message):
     bot.send_message(message.chat.id, str(message.chat.id))
 
-@bot.channel_post_handler(content_types = ["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "location", "contact",
-                 "new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo",
-                 "group_chat_created", "supergroup_chat_created", "channel_chat_created", "migrate_to_chat_id",
-                 "migrate_from_chat_id", "pinned_message"])
+@bot.channel_post_handler(content_types=types.ContentType.ANY])
 def reposts(message):
     text = ""
 #     print(str(message.forward_from_chat.id))
