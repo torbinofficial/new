f030f4bf2b0118c63d3e473420660d07416f1894
Vyacheslav <45356117+torbinofficial@users.noreply.github.com>
2022-01-31 16:29:36 +0200

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -281,11 +281,11 @@ def reposts(message):
             for index, row in channels.iterrows():
                 if(row['id канала'] == message.chat.id):
                     print("THIRD IF")
-                    text += str(row['Тег админа']) + " " + str(row['Тег канала']) + " репостнул "
+                    text += str(row['Тег админа']).replace(' ', '') + " " + str(row['Тег канала']).replace(' ', '') + " репостнул "
             for index, row in channels.iterrows():
                 if(row['id канала'] == message.forward_from_chat.id):
                     print("FOURTH IF")
-                    text += str(row['Тег канала']) + " " + str(row['Тег админа']) 
+                    text += str(row['Тег канала']).replace(' ', '') + " " + str(row['Тег админа']).replace(' ', '')
             for index, row in chats.iterrows():
                 print("FIFTH IF")
                 bot.send_message(chat_id = row['id'], text = text)
